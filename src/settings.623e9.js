b2adba1e3b6c2207cf1cc51b5d4652de7e459637
crytion111 <89855591+crytion111@users.noreply.github.com>
2023-08-29 11:23:17 +0800

    122
    211

---
+++ src/settings.623e9.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/main.fire",orientation:"landscape",jsList:[],bundleVers:{resources:"09ec0",internal:"52d36",main:"f5ff0"}};
